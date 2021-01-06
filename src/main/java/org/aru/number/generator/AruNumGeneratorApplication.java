72c30a844fb4fc118d2ee32eb3aabe986f6413de
srinivasbanoth1988 <49346752+srinivasbanoth1988@users.noreply.github.com>
2021-01-06 16:25:11 +0530

    Initial commit

---
+++ src/main/java/org/aru/number/generator/AruNumGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.aru.number.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AruNumGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AruNumGeneratorApplication.class, args);
+	}
+
+}
